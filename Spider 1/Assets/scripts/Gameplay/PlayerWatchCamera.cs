0f70edb1736141db4d0d2eed9c9ad85543514ea2
aovlexus <aovlexus@gmail.com>
2019-03-31 11:25:55 +0700

    Fix 2doors level

---
+++ Spider 1/Assets/scripts/Gameplay/PlayerWatchCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    [RequireComponent(typeof(Camera))]
+    public class PlayerWatchCamera : MonoBehaviour
+    {
+        private Camera _camera;
+        [SerializeField] private float maxDistance = 10f;
+        [SerializeField] private GameObject player;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            var position = _camera.transform.position;
+            var cameraProjection = new Vector3(position.x, position.y, 0);
+            var dif = cameraProjection - player.transform.position;
+            if (!(dif.sqrMagnitude >= maxDistance)) return;
+
+            var newPosition = player.transform.position + Vector3.ClampMagnitude(dif, maxDistance);
+            var transform1 = _camera.transform;
+            transform1.position = new Vector3(newPosition.x, newPosition.y, transform1.position.z);
+        }
+    }
+}
